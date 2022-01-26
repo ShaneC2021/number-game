0b3e8d717c96e24aaa0ce89434d457ba85255fe0
ShaneC2021 <84458099+ShaneC2021@users.noreply.github.com>
2022-01-25 21:16:11 -0330

    update

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ let counter = 0;
 let randomNumber = 0;
 
 function computerChooses() { 
-  randomNumber = Math.ceil(Math.random() * 100) + 1; //rounds decimal numbers up to nearest integer, max number is 100
-  document.getElementById("computer").innerText = 
-  "Computer has chosen a number which will be revealed at the end of the game";
+  randomNumber = Math.ceil(Math.random() * 99) + 1; //rounds decimal numbers up to nearest integer, max number is 100
+ 
 }
 
 function compare() { // comparison function
@@ -18,27 +17,26 @@ function compare() { // comparison function
   let userGuess = parseFloat(document.getElementById("guess").value);
 
   if(( counter <= 3) && (userGuess === randomNumber)) { // if correct number is chosen and guesses are < 3
-    alert("You win !!! Hit ok to play agains");                               //User wins
+    alert("You win !!! Hit ok to play again");         // user wins
     document.getElementById("myForm").reset();                                 
     counter = 0 ;
     computerChooses();                                  // new random number is chosen
     return false;                                       // exit function
-}
+  }
   if ((counter === 3) && (userGuess !== randomNumber)) { // if guess === 3 and still no correct answer you lose
     alert("Computer's number was  " + randomNumber + " YOU LOSE   womp womp womp  :(  Hit ok to play again");
     counter = 0;
     document.getElementById("myForm").reset();
     computerChooses();                                  // new random number is chosen
-    return false;                                       //exit function
-    
-}
+    return false;                                       // exit function
+  }
   if (userGuess < randomNumber) {
     alert("Aim higher");
     document.getElementById("myForm").reset();
-}
+  }
 
   if(userGuess > randomNumber) {
     alert("Aim lower");
     document.getElementById("myForm").reset();
-}
+  }
 }
